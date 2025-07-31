2dbdb64c2573a598c36373a5c56aee9971105f98
jackpencwby <kajonyod.jack@gmail.com>
2025-07-31 10:09:18 +0700

    first commit

---
+++ src/test/java/com/softdev/softdev/SoftdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softdev.softdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
