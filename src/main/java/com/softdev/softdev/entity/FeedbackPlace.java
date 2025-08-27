abd9e7967c279cb91a3d45ec8f542195a25f49e3
jackpencwby <kajonyod.jack@gmail.com>
2025-08-27 19:56:00 +0700

    Add FeedbackPlace entity class with JPA annotations

---
+++ src/main/java/com/softdev/softdev/entity/FeedbackPlace.java
@@ -0,0 +1,35 @@
+package com.softdev.softdev.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "feedback_place")
+public class FeedbackPlace {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "feedback_place_id")
+    private Long FeedbackPlaceId;
+
+    @Column(name = "rating", nullable = false)
+    private Integer rating;
+
+    @Column(name = "comment", nullable = true)
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "place_id")
+    private Bus bus;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
