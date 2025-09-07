737839dd43a635eec4babae4e50cf17453dd2ccb
jackpencwby <kajonyod.jack@gmail.com>
2025-09-07 12:35:55 +0700

    implement PlaceController and PlaceService methods for retrieving places by ID and finding nearby places

--- src/main/java/com/softdev/softdev/service/PlaceService.java
+++ src/main/java/com/softdev/softdev/service/PlaceService.java
@@ -1,11 +1,13 @@
 package com.softdev.softdev.service;
 
+import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.softdev.softdev.dto.place.PlaceDTO;
+import com.softdev.softdev.entity.BusStop;
 import com.softdev.softdev.entity.Place;
 import com.softdev.softdev.repository.PlaceRepository;
 
@@ -14,6 +16,16 @@ public class PlaceService {
     @Autowired
     private PlaceRepository placeRepository;
 
+    @Autowired
+    private GeolocationService geolocationService;
+
+    @Autowired 
+    private BusStopService busStopService;
+
+    public Place getPlaceById(Long placeId) {
+        return placeRepository.findById(placeId).orElseThrow(() -> new RuntimeException("Place not found for placeId: " + placeId));
+    }
+
     public List<Place> getPlaceByRouteId(Long routeId) {
         return placeRepository.findAllByBusStopRouteRouteId(routeId).orElseThrow(() -> new RuntimeException("Place not found for routeId: " + routeId));
     }
@@ -21,6 +33,18 @@ public class PlaceService {
     public List<Place> getPlaceByStopId(Long stopId) {
         return placeRepository.findAllByBusStopBusStopId(stopId).orElseThrow(() -> new RuntimeException("Place not found for stopId: " + stopId));
     }
+
+    public List<Place> findNearByPlace(Double userLatitude, Double userLongitude, Long routeId) {
+        List<BusStop> busStops = busStopService.findAllByRouteRouteId(routeId);
+
+        BusStop nearestBusStop = busStops.stream()
+            .min(Comparator.comparingDouble(busStop -> geolocationService.haversine(userLatitude, userLongitude, busStop.getGeoLocation().getLatitude(), busStop.getGeoLocation().getLongitude())))
+            .orElseThrow(() -> new RuntimeException("No busStops found"));
+        
+        List<Place> nearByPlace = getPlaceByStopId(nearestBusStop.getBusStopId());
+
+        return nearByPlace;
+    } 
 
     public PlaceDTO toDto(Place place) {
         PlaceDTO dto = new PlaceDTO();
