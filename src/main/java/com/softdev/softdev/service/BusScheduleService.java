a3be70c917b0556d9e806a4c784e37e88c995759
Thanabodee <1847123@gmail.com>
2025-10-03 16:57:52 +0700

    create DTO for bus schedule on bus driver page.

--- src/main/java/com/softdev/softdev/service/BusScheduleService.java
+++ src/main/java/com/softdev/softdev/service/BusScheduleService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.softdev.softdev.dto.bus_driver.BusScheduleDTO;
 import com.softdev.softdev.entity.BusSchedule;
 import com.softdev.softdev.repository.BusScheduleRepository;
 
@@ -15,5 +16,18 @@ public class BusScheduleService {
 
     public List<BusSchedule> findBusScheduleByBusId(Long busId) {
         return busScheduleRepository.findByBusBusIdOrderByRoundAsc(busId).orElseThrow(() -> new RuntimeException("BusSchedule not found for busId: " + busId));
+    }
+
+    public BusScheduleDTO toDto(BusSchedule busSchedule) {
+        BusScheduleDTO dto = new BusScheduleDTO();
+        dto.setOrder(busSchedule.getScheduleOrder());
+        dto.setArriveTime(busSchedule.getArriveTime());
+        dto.setRound(busSchedule.getRound());
+        dto.setBusStopName(busSchedule.getBusStop().getName());
+        return dto;
+    }
+
+    public List<BusScheduleDTO> toDtos(List<BusSchedule> busSchedules) {
+        return busSchedules.stream().map(this::toDto).toList();
     }
 }
